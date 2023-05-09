0037a6caf645b630d05ca18b8f977c221aeca162
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-05-09 12:22:07 +0100

    commit & push

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './App.css'
+import './main.css'
 import Home from './components/Home'
 const App = () => {
   return (
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export default function Home() {
-  return <div className="h-screen w-full bg-[#0a192f]">Home</div>
+  return <div className="h-screen w-full bg-[#FFF0F5]">Home</div>
 }
